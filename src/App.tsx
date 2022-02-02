65b340d0f1914591532c3204ffe47a562ddbae96
Exequiel Mleziva <nathan.hyan2048@gmail.com>
2022-02-02 16:48:55 -0300

    Force feed auth by now. This will definitevly change

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import { Container } from 'react-bootstrap';
 import NavigationBar from 'components/NavigationBar';
 import { ProductProvider } from 'context/products/ProductContext';
 import ProductList from 'screens/ProductList/ProductList';
+import { login } from 'services/authorization';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    login(12345678, 'asd123123');
+  }, []);
+
   return (
     <ProductProvider>
       <Container fluid>
