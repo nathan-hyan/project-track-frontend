57940af9a111d3b9e95ab94ed1023994510b0902
Exequiel Mleziva <nathan.hyan2048@gmail.com>
2022-02-22 10:02:01 -0300

    Added Costo field

--- src/screens/ProductList/components/constants.ts
+++ src/screens/ProductList/components/constants.ts
@@ -35,12 +35,18 @@ export const PRODUCT_FORM: ProductForm[] = [
   },
   {
     id: 2,
+    label: 'Costo',
+    name: 'cost',
+    type: InputType.Number,
+  },
+  {
+    id: 3,
     label: 'Stock',
     name: 'stock',
     type: InputType.Number,
   },
   {
-    id: 3,
+    id: 4,
     label: 'Código de barras',
     name: 'barcode',
     type: InputType.Text,
